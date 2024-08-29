beaf5bcf4120130c665279be9170c09a76ddf7ad
Nada <nada1nadiri@gmail.com>
2024-08-29 08:08:28 +0200

    "update"

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -19,12 +19,8 @@ namespace MyApp.Controllers
         }
 
         public async Task<IActionResult> Index()
-        {
-            if (!User.Identity.IsAuthenticated)
-            {
-                // Redirige vers la page de connexion
-                return RedirectToAction("Login", "Account", new { area = "Identity" });
-            }
+        {   
+
             // Fetch products from the database
             var products = await _context.Product.ToListAsync();
 
